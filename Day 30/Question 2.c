41ce657b99c2c9a74837273c4dcb67948972b458
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-05-03 15:45:22 +0530

    Update Question 2.c

---
+++ Day 30/Question 2.c
@@ -0,0 +1,40 @@
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    struct ListNode* next;
+    while (curr) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    l1 = reverseList(l1);
+    l2 = reverseList(l2);
+    
+    struct ListNode* result = NULL;
+    int carry = 0;
+    
+    while (l1 || l2 || carry) {
+        int sum = carry;
+        if (l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        
+        carry = sum / 10;
+        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newNode->val = sum % 10;
+        newNode->next = result;
+        result = newNode;
+    }
+    
+    return result;
+}
