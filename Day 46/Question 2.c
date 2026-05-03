b4b36c892ef93276bbc8c2e39590f04437a9b0f7
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-05-03 16:10:29 +0530

    Update Question 2.c

--- Day 46/Question 2.c
+++ Day 46/Question 2.c
@@ -1,4 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
+int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
+    *returnSize = 0;
+    if (!root) return NULL;
 
-// Binary Tree Level Order Traversal solution in C
+    int maxLevels = 2001;
+    int** result = (int**)malloc(maxLevels * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(maxLevels * sizeof(int));
+
+    struct TreeNode** queue = (struct TreeNode**)malloc(maxLevels * sizeof(struct TreeNode*));
+    int front = 0, back = 0;
+    queue[back++] = root;
+
+    while (front < back) {
+        int levelSize = back - front;
+        result[*returnSize] = (int*)malloc(levelSize * sizeof(int));
+        (*returnColumnSizes)[*returnSize] = levelSize;
+
+        for (int i = 0; i < levelSize; i++) {
+            struct TreeNode* node = queue[front++];
+            result[*returnSize][i] = node->val;
+
+            if (node->left)  queue[back++] = node->left;
+            if (node->right) queue[back++] = node->right;
+        }
+
+        (*returnSize)++;
+    }
+
+    free(queue);
+    return result;
+}
